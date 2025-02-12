d6111f087483bf218c5d223f60649e2f7063ae1e
dldri <diovian.liu@gmail.com>
2025-02-12 16:26:09 +0800

    created default layout 35:16

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+  const prismic = usePrismic()
+
+  const { data: settings } = await useAsyncData(() => prismic.client.getSingle('settings'))
+  
+  useSeoMeta
+</script>
+
+<template>
+  <div>
+    {{ settings }}
+    <slot />
+  </div>
+</template>
