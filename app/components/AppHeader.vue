d0101ac768d83896b51e61f631117ddbb62a5033
Dio Luwandri <diovian.liu@gmail.com>
2025-02-25 05:48:05 +0800

    added AppHeader.vue 47:31

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import type { Content } from "@prismicio/client"
+
+defineProps<{
+	settings?: Content.SettingsDocument
+}>()
+</script>
+
+<template>
+	<header>Header Content</header>
+</template>
